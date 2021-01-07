1d327e6081eca268f694c14498bba0e1925cb26f
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-07 11:33:03 -0600

    Membership table modifications

--- Migrations/202101071711222_PopulateMembershipTypes.cs
+++ Migrations/202101071711222_PopulateMembershipTypes.cs
@@ -11,8 +11,6 @@ namespace Vidly.Migrations
             Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(2, 30, 1, 10)");
             Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(3, 90, 3, 15)");
             Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(4, 300, 12, 20)");
-
-
         }
 
         public override void Down()
