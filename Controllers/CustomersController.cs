3e376ee68f9f0148083d54897c1e51635751fed2
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-07 11:02:32 -0600

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(GetCustomers());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(cust => cust.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "Oscar"},
+                new Customer { Id = 2, Name = "Pedro"}
+            };
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(GetMovies());
+        }
+
+        //Attribute routing
+        [Route("movies/released/{year}/{month:regex(\\d{{2}}$):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        private List<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Spider-Man" }
+            };
+        }
+    }
+}
